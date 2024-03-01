1ca9dbfc63303360421de28383f3b784fcf16094
DevonPartridgeOfficer <partda1@student.op.ac.nz>
2024-03-02 12:28:13 +1300

    ADD: (1.0) Jupyter Tutorial -- Initial Work and file creation

---
+++ 1.0_Jupyter_Tutorial.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  2 12:21:42 2024
+
+@author: Devon Henderson
+"""
+
+## Introduction to Jupyter Notebooks
+## Week 1 - Lecture 1
+
+#### Simple print statement
+a = 10
+print(a)
+
+#### Managing the IPython Kernel
+import time
+#time.sleep(10) #Commented to remove wait time
+
+#### Crash Python to restart kernel
+# DO NOT UNCOMMENT THIS
+# import os
+#os._exit(0)
+ 
+### stdout and stderr
+
+
