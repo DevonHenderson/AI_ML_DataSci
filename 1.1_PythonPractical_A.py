30ae0ab9b2da6031054a437b763e713604d96dee
DevonPartridgeOfficer <partda1@student.op.ac.nz>
2024-03-02 13:16:51 +1300

    ADD: (1.1) Python Practical A -- file creation

---
+++ 1.1_PythonPractical_A.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  2 13:14:29 2024
+
+@author: devon
+"""
+
+## Python Practical A
+## Week 1 - Lecture 1
+
+### Basic Print Statement
+print("Hello World")
+
