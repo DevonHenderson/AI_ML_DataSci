447a62dd76643a985d5aa9ad60358fbaa8b00f7e
Devon Henderson (1000103794) <PARTDA1@student.op.ac.nz>
2024-03-05 10:50:30 +1300

    ADD: (1.2) Custom import files support.py, overloading and data hiding

--- 1.2_PythonPractical_B.py
+++ 1.2_PythonPractical_B.py
@@ -38,13 +38,14 @@ del(emp1.age)
 print(hasattr(emp1, "age")) #Should return false as "age" deleted
 print(hasattr(emp2, "name"))
 
+########################################################################
 print("\nBUILT-IN CLASS ATTRIBUTES")
 print(emp1.__dict__) #Dict containing the classes namespace
 print(emp1.__doc__)  #Class documentation string (none if undefined)
 print(emp1.__class__.__name__) #Class name
 print(emp1.__module__) #Module(library) name in which the class is defined
 
-
+########################################################################
 print("\nCLASS INHERITANCE")
 class Parent:
     parentAttr = 100
@@ -60,6 +61,9 @@ class Parent:
         
     def getAttr(self):
         print("Parent attribute: ", Parent.parentAttr)
+    
+    def overrideTest(self):
+        print('Calling parent method')
         
 class Child(Parent): #define child to inherit from Parent class
     def __init__(self):
@@ -69,12 +73,65 @@ class Child(Parent): #define child to inherit from Parent class
     def childMethod(self):
         print("Calling child method")
         
+    def overrideTest(self): # override parent method
+        print('Calling child method')
+        
 c=Child()
-print("--------------")
+print("---")
 c.childMethod()
 c.parentMethod()
 c.setAttr(200)
 c.getAttr()
+print("---")
+c = Child()          # instance of child
+c.overrideTest()         # child calls overridden method
+## Both parent and child have overrideTest but only child.overrideTest will call
+
+########################################################################
+### Vector Overloading Example
+print("\nVECTOR OVERLOAD PARAMS")
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def __str__(self): #Overloading print method
+        return('Vector (%d, %d)' % (self.x, self.y))
+    
+    def __add__(self, other):  #Overloading addition method
+        return Vector(self.x + other.x, self.y + other.y)
+
+a = Vector(4, 2)
+b = Vector(1, 5)
+c = a + b # plus operator has been overloaded by the vector class implementation
+print(c)  # the vector class also overloaded the print method
+
+
+########################################################################
+### Data Hiding 
+print("\nPUBLIC / PRIVATE ATTRIBUTES SCOPE")
+class JustCounter:
+    __secretCount = 0 #__ hides attribute outside class
+    publicAttr = "This is a public attribute"
+    
+    def count(self):
+        self.__secretCount += 1
+        print(self.__secretCount)
+        
+counter = JustCounter()
+counter.count()
+counter.count()
+print(counter.publicAttr)
+# print(counter.__secretCount) #Error for trying to access private attr
+
+
+########################################################################
+### Using other files as modules with import
+print("\nIMPORTING FUNCTIONS FROM FILES")
+import support #In same filepath called support.py
+support.print_func("Devon")
+support.squared_Func(10)
+
 ########################################################################
 #END OF FILE
 ########################################################################
