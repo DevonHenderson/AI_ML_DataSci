97a3080cf3d9e32c53f2ece78e26d57ce5d12c81
Devon Henderson (1000103794) <PARTDA1@student.op.ac.nz>
2024-03-13 11:17:23 +1300

    ADD: (5.1) Solutions added for various questions in word doc

--- 5.1_Probability_and_statistics_fundamentals.py
+++ 5.1_Probability_and_statistics_fundamentals.py
@@ -8,4 +8,17 @@ Probability and statistics fundamentals
 Week 3 Lecture 1
 """
 ########################################################################
-### 
+### Probability & Statistics Overview - Word Doc
+print("Question 1")
+print("Roll 1 die = 3 : ")
+print("Roll 1 die = odd : ")
+print("Roll 2 die = [1,1] : " )
+print("Roll 2 die = [1,3] : " )
+
+print("Question 2")
+print("a): P(B∩Male) : ", 10/100)
+print("b): P(male) : ", 50/100)
+print("c): P(female) : ", 25/35)
+
+print("Mean: ", (5 + 6 + 1 + 3 + 8 + 9 + 2 + 3 + 3 + 4 + 9)/11)
+print("[1,3] die roll: ", 1/36)
