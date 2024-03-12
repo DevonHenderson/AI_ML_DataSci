855dd79ba5c1154d96bb159100f278924b9cff90
Devon Henderson (1000103794) <PARTDA1@student.op.ac.nz>
2024-03-13 10:02:04 +1300

    ADD: (5.1) File for practical created

---
+++ 5.1_Probability_and_statistics_fundamentals.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 13 10:00:06 2024
+
+@author: Devon Henderson
+
+Probability and statistics fundamentals
+Week 3 Lecture 1
+"""
+########################################################################
+### 
